b2ab6762689dc3ae2468685f95f1b38e84e16fad
Benedikt Baumgarten <baumgarten.benedikt@icloud.com>
2026-01-31 17:05:51 +0100

    Implemented dialogue boxes (test by klicking BURN THEM)

--- scripts/add.gd
+++ scripts/add.gd
@@ -5,3 +5,5 @@ extends Button
 func _pressed() -> void:
 	bar.internalDummyValue = bar.value + 0.1
 	bar.update()
+	
+	DialogManager.start_dialog(global_position, ["Nooo!", "This hurts so much.", "Hey, why would you do this, stop it :(", "Ahhhhhhhhhhhhhhhhhhhhh I'm screaming so loooooooooong not just because I wanna see if the linebreak works nono. Tho actually Im kinda curious now what a coincidence, does it work? Orrr not? I can't really know as I'm writing this soooooo"])
