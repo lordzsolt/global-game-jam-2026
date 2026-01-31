bdb17ca1091e39835092177a693766a3d17ea00c
Benedikt Baumgarten <baumgarten.benedikt@icloud.com>
2026-01-31 01:23:33 +0100

    Added UI for Tug of War Bar

---
+++ scenes/bar.gd
@@ -0,0 +1,13 @@
+extends HSlider
+
+var internalDummyValue : int = 50
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#Put here sth like player.conversationLevelChanged.connect(update)
+	update()
+
+
+
+func update() -> void:
+	value = 50 #dummy value, actually put here sth like "player.conversationLevel"
