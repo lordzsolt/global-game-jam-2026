481b89d7ad9ad849ebcd7a288402ca63884592e9
Benedikt Baumgarten <baumgarten.benedikt@icloud.com>
2026-02-01 01:58:49 +0100

    Fixed demo bar buttons

--- scripts/add.gd
+++ scripts/add.gd
@@ -3,9 +3,4 @@ extends Button
 @export var bar: HSlider
 
 func _pressed() -> void:
-	bar.internalDummyValue = bar.value + 0.1
-	
-	print("Button Global Pos:" + str(global_position))
-	#bar.internalDummyValue = bar.value + 0.1
-	#bar.update()
 	GState.health += 0.1
--- scripts/substract.gd
+++ scripts/substract.gd
@@ -3,8 +3,4 @@ extends Button
 @export var bar: HSlider
 
 func _pressed() -> void:
-	#bar.internalDummyValue = bar.value - 0.1
-	#bar.update()
 	GState.health -= 0.1
-	
-	
